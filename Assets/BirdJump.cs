a345799893529410e53539e34904bdbf11a1fe2d
e-lumos <e_lumos@kakao.com>
2024-03-19 23:01:58 +0900

    Move to game over scene

--- Assets/BirdJump.cs
+++ Assets/BirdJump.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdJump : MonoBehaviour
 {
@@ -23,4 +24,8 @@ public class BirdJump : MonoBehaviour
 
 		}
     }
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+        SceneManager.LoadScene("GameOverScene");
+	}
 }
